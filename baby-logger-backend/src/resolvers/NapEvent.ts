b2a5a41b9cb3944a61132d0bf1b15ff57011c5dc
Ilja Cooreman <ilja.cooreman@imec.be>
2019-07-19 08:42:36 +0200

    add duration in backend

--- baby-logger-backend/src/resolvers/NapEvent.ts
+++ baby-logger-backend/src/resolvers/NapEvent.ts
@@ -1,7 +1,14 @@
 import { Context } from '../utils';
 
+import moment = require('moment');
+
 export const NapEvent = {
   baby: ({ id }, args, ctx: Context) => {
     return ctx.prisma.napEvent({ id }).baby()
+  },
+  duration: async ({ id }, args, ctx: Context) => {
+    const napEvent = await ctx.prisma.napEvent({ id })
+    console.log('napevent', napEvent)
+    return napEvent.end ? moment(napEvent.end).diff(napEvent.start, 'seconds') : undefined
   },
 }
