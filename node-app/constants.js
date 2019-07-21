d078158e6d1b772f733f7e852eecb42e3078dd97
Ilja Cooreman <ilja.cooreman@imec.be>
2019-07-22 00:51:22 +0200

    live mode

--- node-app/constants.js
+++ node-app/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   ENDPOINT: 'https://logger-backend.azurewebsites.net/',
   // ENDPOINT: 'http://localhost:4000',
-  // BABY_ID: 'cjy7h86o4jbhx0b53snlyrgss' // the real luz
-  BABY_ID: 'cjydb14i3rjvg0b36fi6e717j' // testluz
+  BABY_ID: 'cjy7h86o4jbhx0b53snlyrgss' // the real luz
+  // BABY_ID: 'cjydb14i3rjvg0b36fi6e717j' // testluz
 }
