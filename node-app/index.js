4f00fe5ae310015536dc212cd12eff335a4f584d
Ilja Cooreman <ilja.cooreman@imec.be>
2019-07-21 21:34:20 +0200

    try out button

--- node-app/index.js
+++ node-app/index.js
@@ -1,24 +1,10 @@
 const readline = require('readline');
 const { BABY_ID } = require('./constants')
 var Gpio = require('onoff').Gpio;
-var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
-function blinkLED() { //function to start blinking
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
-}
+const button = new Gpio(4, 'in', 'both');
 
-function endBlink() { //function to stop blinking
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport GPIO to free resources
-}
-
-setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+button.watch((err, value) => { console.log('value', value); console.log(err) });
 
 const { graphqlRequest } = require('./graphqlRequest');
 
