8f7a47b5d548a98d0d2606662a3d7ca94dde80f0
= <=>
2019-08-03 10:58:31 +0200

    change user on start:dev (untested)

--- node-app/src/constants.js
+++ node-app/src/constants.js
@@ -1,5 +1,5 @@
 
 export const ENDPOINT = 'https://logger-backend.azurewebsites.net/';
 // export const ENDPOINT ='http://localhost:4000';
-export const BABY_ID = 'cjy7h86o4jbhx0b53snlyrgss'; // the real luz
+export const BABY_ID = process.env.BABY_ID == `development` ? 'cjydb14i3rjvg0b36fi6e717j' : 'cjy7h86o4jbhx0b53snlyrgss';
   // export const BABY_ID ='cjydb14i3rjvg0b36fi6e717j'; // testluz
