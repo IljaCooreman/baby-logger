ce4678474ce897ed9786576a087d2cd4d41731e6
Ilja Cooreman <iljaCooreman@LAPTOP-1707271.local>
2019-08-09 09:54:10 +0200

    remove console.log

--- src/components/EventListContainer.js
+++ src/components/EventListContainer.js
@@ -7,7 +7,6 @@ import EventListItem from './EventListItem';
 const EventListContainer = ({events, isVisible, refetch}) => {
     if (!isVisible) return null;
     const sortedEvents = [...events].sort((a, b) => new Date(b.start) - new Date(a.start))
-    console.log(events, sortedEvents)
     return (
         <div>
         {
